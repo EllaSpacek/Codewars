aac088b011123a726c76fb146e5f659a69000531
EllaSpacek <65894512+EllaSpacek@users.noreply.github.com>
2023-04-05 18:31:36 +0200

    Added exercise, 5kyu

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -603,7 +603,9 @@ string orderWeight1(string strng)
         });
     }
 
-    return String.Join(' ', listOfWeights.OrderBy(x => x.weightDigitSum).ThenBy(x => x.weightStr).Select(x => x.weightStr));
+    return String.Join(' ', listOfWeights.OrderBy(x => x.weightDigitSum)
+                                         .ThenBy(x => x.weightStr)
+                                         .Select(x => x.weightStr));
 }
 
 string orderWeight2(string strng)
@@ -614,17 +616,121 @@ string orderWeight2(string strng)
         .ThenBy(n => n));
 }
 
-string orderWeight3(string strng)
-    => string.Join(" ", strng.Split().OrderBy(s => s.Sum(char.GetNumericValue)).ThenBy(x => x));
+//Console.WriteLine(orderWeight1("103 123 4444 99 2000"));
+//Console.WriteLine(orderWeight1("2000 10003 1234000 44444444 9999 11 11 22 123"));
+//Console.WriteLine(orderWeight2("103 123 4444 99 2000"));
+//Console.WriteLine(orderWeight2("2000 10003 1234000 44444444 9999 11 11 22 123"));
 
-Console.WriteLine(orderWeight1("103 123 4444 99 2000"));
-Console.WriteLine(orderWeight1("2000 10003 1234000 44444444 9999 11 11 22 123"));
-Console.WriteLine(orderWeight2("103 123 4444 99 2000"));
-Console.WriteLine(orderWeight2("2000 10003 1234000 44444444 9999 11 11 22 123"));
-Console.WriteLine(orderWeight3("103 123 4444 99 2000"));
-Console.WriteLine(orderWeight3("2000 10003 1234000 44444444 9999 11 11 22 123"));
+/////////////////////////////////////         5kyu         ///////////////////////////////////////////
 
+/////////////////////////////////////       05.04.23      ///////////////////////////////////////////
 
+/*
+    For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array.
+
+    The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+
+    The following are some examples of how this class is used:
+
+    var helper = new PaginationHelper<char>(new List<char>{'a', 'b', 'c', 'd', 'e', 'f'}, 4);
+    helper.PageCount(); //should == 2
+    helper.ItemCount(); //should == 6
+    helper.PageItemCount(0); //should == 4
+    helper.PageItemCount(1); // last page - should == 2
+    helper.PageItemCount(2); // should == -1 since the page is invalid
+
+    // pageIndex takes an item index and returns the page that it belongs on
+    helper.PageIndex(5); //should == 1 (zero based index)
+    helper.PageIndex(2); //should == 0
+    helper.PageIndex(20); //should == -1
+    helper.PageIndex(-10); //should == -1
+*/
+
+List<char> collection = new() { 'a', 'b', 'c', 'd', 'e', 'f' };
+PagnationHelper<char> helper = new(collection, 4);
+Console.WriteLine(helper.PageCount);
+Console.WriteLine(helper.ItemCount);
+Console.WriteLine(helper.PageItemCount(0));
+Console.WriteLine(helper.PageItemCount(1));
+Console.WriteLine(helper.PageItemCount(2));
+
+public class PagnationHelper<T>
+{
+    private IList<T> collection;
+    private int itemsPerPage;
+
+    /// <summary>
+    /// Constructor, takes in a list of items and the number of items that fit within a single page
+    /// </summary>
+    /// <param name="collection">A list of items</param>
+    /// <param name="itemsPerPage">The number of items that fit within a single page</param>
+    public PagnationHelper(IList<T> collection, int itemsPerPage)
+    {
+        this.collection = collection;
+        this.itemsPerPage = itemsPerPage;
+    }
+
+    /// <summary>
+    /// The number of items within the collection
+    /// </summary>
+    public int ItemCount
+    {
+        get
+        {
+            return this.collection.Count;
+        }
+    }
+
+    /// <summary>
+    /// The number of pages
+    /// </summary>
+    public int PageCount
+    {
+        get
+        {
+            return this.ItemCount % this.itemsPerPage == 0 ? this.ItemCount / this.itemsPerPage : this.ItemCount / this.itemsPerPage + 1;
+        }
+    }
+
+    /// <summary>
+    /// Returns the number of items in the page at the given page index 
+    /// </summary>
+    /// <param name="pageIndex">The zero-based page index to get the number of items for</param>
+    /// <returns>The number of items on the specified page or -1 for pageIndex values that are out of range</returns>
+    public int PageItemCount(int pageIndex)
+    {
+        if (pageIndex > this.PageCount - 1 || pageIndex < 0)
+        {
+            return -1;
+        }
+        else if (this.ItemCount % this.itemsPerPage == 0 || pageIndex != PageCount - 1)
+        {
+            return this.itemsPerPage;
+        }
+        else
+        {
+            return this.ItemCount - (this.itemsPerPage * (this.PageCount - 1));
+            // or this.ItemCount % this.itemsPerPage;
+        }
+    }
+
+    /// <summary>
+    /// Returns the page index of the page containing the item at the given item index.
+    /// </summary>
+    /// <param name="itemIndex">The zero-based index of the item to get the pageIndex for</param>
+    /// <returns>The zero-based page index of the page containing the item at the given item index or -1 if the item index is out of range</returns>
+    public int PageIndex(int itemIndex)
+    {
+        if (itemIndex > this.ItemCount - 1 || itemIndex < 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return itemIndex / this.itemsPerPage;
+        }
+    }
+}
 
 class Weight
 {
